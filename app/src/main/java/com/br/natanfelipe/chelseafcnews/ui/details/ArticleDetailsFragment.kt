483fa050b6a68a8ff555726d74210082a1b7f18d
natanbrito <natanbrito.mtm@outlook.com>
2020-04-25 19:18:57 -0300

    Sharing link is working

--- app/src/main/java/com/br/natanfelipe/chelseafcnews/ui/details/ArticleDetailsFragment.kt
+++ app/src/main/java/com/br/natanfelipe/chelseafcnews/ui/details/ArticleDetailsFragment.kt
@@ -1,5 +1,6 @@
 package com.br.natanfelipe.chelseafcnews.ui.details
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -47,5 +48,22 @@ class ArticleDetailsFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         toolbar.setNavigationOnClickListener { appCompatActivity.onBackPressed() }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab.setOnClickListener {
+            createShareIntent()
+        }
+    }
+
+    private fun createShareIntent() {
+        val intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, viewModel.link)
+            type = "text/plain"
+        }
+        val shareIntent = Intent.createChooser(intent, null)
+        startActivity(shareIntent)
     }
 }
